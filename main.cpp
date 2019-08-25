d21d88607c6977303bd53e5cd85e8fa7814c6224
qqwqqk <qqwqqk@shu.edu.cn>
2019-08-25 21:10:26 +0800

    LFM UPDATE FOUR

--- main.cpp
+++ main.cpp
@@ -7,9 +7,9 @@ using namespace std;
 
 int main()
 {
-  string name = "Metadata_AC";
+  string name = "Metadata_WR";
   char intercept = 'E';
-  int number = 99999;
+  int number = 10000;
   bool connected = true;
   bool sequence = true;
   char nodetype = 'A';
