73eaf47e7e43572b81b82ff0e44b98502fb723c7
qqwqqk <qqwqqk@shu.edu.cn>
2019-08-25 20:42:29 +0800

    COPRA UPDATE

--- main.cpp
+++ main.cpp
@@ -7,9 +7,9 @@ using namespace std;
 
 int main()
 {
-  string name = "Metadata_MC";
-  char intercept = 'B';
-  int number = 800;
+  string name = "Metadata_WR";
+  char intercept = 'E';
+  int number = 10000;
   bool connected = true;
   bool sequence = true;
   char nodetype = 'A';
