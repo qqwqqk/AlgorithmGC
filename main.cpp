6dc4abd7f23992be9fb112ea0b5cddd6e3fbb130
qqwqqk <qqwqqk@shu.edu.cn>
2019-08-25 19:17:14 +0800

    non_overlap

--- main.cpp
+++ main.cpp
@@ -6,9 +6,9 @@ using namespace std;
 
 int main()
 {
-  string name = "Metadata_PA";
+  string name = "Metadata_WR";
   char intercept = 'E'; 
-  int number = 5000;
+  int number = 10000;
   bool connected = true;
   bool sequence = true;
   char nodetype = 'A';
