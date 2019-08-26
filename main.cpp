60a0f3bed1d60e775dd88f605dca320c38626487
qqwqqk <qqwqqk@shu.edu.cn>
2019-08-26 10:07:06 +0800

    gc update end

--- main.cpp
+++ main.cpp
@@ -6,9 +6,9 @@ using namespace std;
 
 int main()
 {
-  string name = "Metadata_WR";
+  string name = "Metadata_MC";
   char intercept = 'E'; 
-  int number = 3000;
+  int number = 10000;
   bool connected = true;
   bool sequence = true;
   char nodetype = 'A';
